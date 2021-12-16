183e5ed92d8f21f715c4dc8d131eb417fba5bb5f
jan146 <anurjan@gmail.com>
2021-12-16 18:29:26 +0100

    Added a minimal desktop environment

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export DEBIAN_FRONTEND="noninteractive" # Disable installation tui prompts
+
 # update mirrors
 apt update
 
@@ -13,6 +15,28 @@ ca-certificates curl gnupg2 software-properties-common
 # install gns3 server and gui via pip
 pip3 install gns3-server==2.2.27
 pip3 install gns3-gui==2.2.27
+
+# xfce (minimal)
+apt install -y \
+    geany \
+    xfce4 \
+    libxfce4ui-utils \
+    thunar \
+    midori \
+    xfce4-appfinder \
+    xfce4-panel \
+    xfce4-pulseaudio-plugin \
+    xfce4-whiskermenu-plugin \
+    xfce4-session \
+    xfce4-settings \
+    xfce4-terminal \
+    xfconf \
+    xfdesktop4 \
+    xfwm4 \
+    adwaita-qt \
+    qt5ct 
+systemctl set-default graphical.target
+systemctl restart lightdm
 
 IPV4_ADDRESS=$(ip a show dev eth0 | grep "inet " | sed 's/.*inet //;s/\/24.*//')
 echo "local ipv4 address: $IPV4_ADDRESS"
